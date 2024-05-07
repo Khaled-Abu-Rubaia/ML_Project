5d8a0ebc0dc43490ccba16144d336e15e44ead68
Khaled-Abu-Rubaia <136169751+Khaled-Abu-Rubaia@users.noreply.github.com>
2024-05-07 21:51:49 +0300

    Update suicide_rates_dataset.py

--- suicide_rates_dataset.py
+++ suicide_rates_dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 import os
 
@@ -26,7 +26,7 @@ data['Sex'] = encoded_values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LinearRegression()
+model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
